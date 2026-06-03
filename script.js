9ee9fb934fa1d67898422b972705942cd11ac01b
Pooja Kada <srilehka.kada@gmail.com>
2026-06-03 16:50:03 +0530

    Fixed certificate images

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
     { id: 'c2', title: 'Google AI Essentials', org: 'Coursera', date: 'Completed 5 Courses', cat: 'AI', link: '', img: '' },
     { id: 'c3', title: 'Full Stack Development Certificate', org: 'Future Interns', date: '2026', cat: 'Development', link: 'https://futureinterns.com/verification/?cin=FIT/MAY26/FS16115', img: 'images/future_Interns_certificate.png' },
     { id: 'c4', title: 'Data Analytics Internship Certificate', org: 'Thiranex', date: '2026', cat: 'Data', link: '', img: '' },
-    { id: 'c5', title: 'Cloud Computing Certificate', org: 'upSkills Camp', date: '2026', cat: 'Cloud', link: 'https://uniconvergetech9893.spayee.com/verify-certificate?serialno=2EY0ZR8G', img: 'images/upskills_camp_certificate.png' },
+    { id: 'c5', title: 'Cloud Computing Certificate', org: 'upSkills Camp', date: '2026', cat: 'Cloud', link: 'https://uniconvergetech9893.spayee.com/verify-certificate?serialno=2EY0ZR8G', img: 'images/upskill_camp_certificate.png' },
     { id: 'c6', title: 'Python Development Certificate', org: 'Yuva Interns', date: '2026', cat: 'Development', link: '', img: '' },
     { id: 'c7', title: 'Data Analytics Job Simulation', org: 'Deloitte (Forage)', date: '2026', cat: 'Simulation', link: 'https://www.theforage.com/simulations/Deloitte%20Australia/data-analytics-s5zy', img: 'images/data_analytics_certificate.png' },
     { id: 'c8', title: 'AWS SimuLearn: Cloud Computing Essentials', org: 'Amazon Web Services', date: '2026', cat: 'Cloud', link: 'https://aws.amazon.com/training/?intClick=gsrc_navbar', img: 'images/AWS_certificate.png' },
